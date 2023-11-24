c6d4afd1196d684f3a84cd397a3ee4b7a2539c87
mhastu <hasenhuetl@student.tugraz.at>
2023-11-24 19:47:13 +0100

    add latching mode for closed inputs during boot

--- README.md
+++ README.md
@@ -15,6 +15,9 @@ It will present itself as a MIDI device.
 When connecting the pins 2-13 to GND, a "note on" signal is sent, and a "note off" signal at disconnect.
 The analog pins 0-3 report analog values via MIDI CC.
 
+When holding down buttons during power-up those buttons will change their behavior
+to a latching note-on/off.
+
 ## Reflashing via Arduino IDE
 The Board will, by default, not be programmable via the Arduino IDE, because it is now a MIDI device.
 To active programming mode, however, the smart USBMidiKliK firmware can set the board into a programmable state again, when receiving following MIDI sysex:
--- ardustomp.ino
+++ ardustomp.ino
@@ -1,4 +1,3 @@
-
 #include <MIDI.h>
 #include <midi_Defs.h>
 #include <midi_Message.h>
@@ -7,35 +6,63 @@
 
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial, midiOut);
 
+// =================================== CONFIG ======================================
+// -------------------- pin selection --------------------
 const uint8_t diPins[] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };  // digital pins to use for note on/off
 const uint8_t anPins[] = { 0, 1, 2 };  // analog pins to use for MIDI control changes
+// -------------------------------------------------------
 
+// ------- digital input contact bounce prevention -------
 const uint8_t diAntiBounceLength = 16;  // number of digital values to store to prevent contact bounce (0-16)
 const uint16_t diHighCompare = 255 << (16-diAntiBounceLength) >> (16-diAntiBounceLength);  // valid value of diAntiBounceBuffer for a digital value change
+// -------------------------------------------------------
+
+// ----------- analog input noise suppression ------------
 const uint8_t anMovingAverage = 4;  // window size of moving average for analog pins to tackle noise
 // moving average is not enough:
 const uint8_t anDirChangeThreshold = 4;  // values to overcome for direction change (in MIDI space, i.e. 0-127)
+// -------------------------------------------------------
+// =================================================================================
 
-// ---------------------- for change detection in loop() ---------------------------
+// =========================== GLOBAL STATE VARIABLES ==============================
+// ---------------- toggle-able states -------------------
+uint8_t noteMode[sizeof(diPins)];  // 0: sent note-on/off represents hardware button state, 1: toggle note-on/off on hardware button press
+uint8_t noteState[sizeof(diPins)];  // current state of toggle-able notes (1=on, 0=off)
+// -------------------------------------------------------
+
+// ----------- for change detection in loop() ------------
 uint8_t diVal[sizeof(diPins)];  // value for each digital pin in current iteration
 uint8_t diLastVal[sizeof(diPins)];  // value for each digital pin in last iteration
-uint16_t diAntiBounceBuffer[sizeof(diPins)];  // last diAntiBounceLength boolean values
 
 uint8_t anVal[sizeof(anPins)];  // value for each analog pin (0-127) in current iteration
 uint8_t anLastVal[sizeof(anPins)];  // value for each analog pin (0-127) in last iteration
 bool anDirUp[sizeof(anPins)];  // current change direction of analog value (0-127). false: down, true: up
-// ---------------------------------------------------------------------------------
+// -------------------------------------------------------
 
-// -------------- for analog sensor value mapping in readInput() -------------------
+// --- for digital sensor value mapping in readInput() ---
+uint16_t diAntiBounceBuffer[sizeof(diPins)];  // last diAntiBounceLength boolean values
+// -------------------------------------------------------
+
+// --- for analog sensor value mapping in readInput() ----
 uint16_t anWindow[sizeof(anPins)][anMovingAverage];  // last anMovingAverage sensor values (0-1023)
 uint8_t anWindowIndex = 0;  // current index in window array
-// ---------------------------------------------------------------------------------
+// -------------------------------------------------------
+// =================================================================================
 
 void setup () {
   for (uint8_t i = 0; i < sizeof(diPins); i++) {
     pinMode(diPins[i], INPUT_PULLUP);
   }
   Serial.begin(31250);
+
+  // set note mode by holding down hardware buttons during boot
+  for (uint8_t i = 0; i < sizeof(diPins); i++) {
+    if (digitalRead(diPins[i]) == LOW) {  // LOW = pressed
+      noteMode[i] = 1;
+    } else {
+      noteMode[i] = 0;
+    }
+  }
 }
 
 // use anVal and diVal to detect change and send MIDI signal
@@ -70,9 +97,21 @@ void loop() {
   for (uint16_t i = 0; i < sizeof(diPins); i++) {
     if (diVal[i] != diLastVal[i]) {
       if (diVal[i] == LOW) {  // note ON at LOW value: Atmega has internal Pullup-Resistors, so GND is being switched
-        midiOut.sendNoteOn(i, 127, 2);
+        if (noteMode[i] == 0) {  // "normal" button mode
+          midiOut.sendNoteOn(i, 127, 2);
+        } else { // toggle-button mode
+          if (noteState[i] == 0) {
+            midiOut.sendNoteOn(i, 127, 2);
+            noteState[i] = 1;
+          } else {
+            midiOut.sendNoteOff(i, 127, 2);
+            noteState[i] = 0;
+          }
+        }
       } else {
-        midiOut.sendNoteOff(i, 127, 2);
+        if (noteMode[i] == 0) {  // "normal" button mode
+          midiOut.sendNoteOff(i, 127, 2);
+        }  // else: nothing to do in toggle mode
       }
       diLastVal[i] = diVal[i];
     }
